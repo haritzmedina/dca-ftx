63f68a37c5fdf655acf42c9c71e1ed1ef49c3532
Haritz <haritzmedina@gmail.com>
2021-11-15 22:57:45 +0100

    Fixed bug in lower than 0 of last 24 change calculation

--- index.js
+++ index.js
@@ -45,8 +45,10 @@ let main = async () => {
                 buyPrice = rounder(price * (1 - (change24h - lessThanCurrentPercentage)), 0, 0)
                 res = await FTX_INSTANCE.createOrder(quantity, pair, 'buy', 'limit', buyPrice)
             } else if (change24h < 0 && lessThanCurrentPercentage > -change24h) { // If it is lower, but still not that low as lessThan, buy in less than price
+                buyPrice = (1 - (lessThanCurrentPercentage + change24h))
                 res = await FTX_INSTANCE.createOrder(quantity, pair, 'buy', 'limit', buyPrice)
             } else { // If is lower than lessThan, directly buy
+                console.log(change24h)
                 res = await FTX_INSTANCE.createOrder(quantity, pair, 'buy', 'market')
             }
             console.log('Pair:' + pair + ' Quantity:' + quantity + ' Price:' + buyPrice)
